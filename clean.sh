7aac9b4b1ed5880675db3fa0b98249432b5e480a
dekuNukem <hammondmay@gmail.com>
2016-12-26 18:21:09 +0800

    doc update

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
 find . -type f -name "*.b#*" -exec rm -f {} \;
 find . -type f -name "*.s#*" -exec rm -f {} \;
 find . -type f -name "*.l#*" -exec rm -f {} \;
+find . -type f -name "*.DS_Store*" -exec rm -f {} \;
+
