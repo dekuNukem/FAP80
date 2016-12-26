99c84fa3bdd3aaa25dad35588f0459997352b5d3
dekuNukem <hammondmay@gmail.com>
2016-12-27 02:09:18 +0800

    clean.sh deletes ds_store

--- clean.sh
+++ clean.sh
@@ -2,4 +2,3 @@ find . -type f -name "*.b#*" -exec rm -f {} \;
 find . -type f -name "*.s#*" -exec rm -f {} \;
 find . -type f -name "*.l#*" -exec rm -f {} \;
 find . -type f -name "*.DS_Store*" -exec rm -f {} \;
-
